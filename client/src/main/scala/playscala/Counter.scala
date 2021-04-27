d4ed057c188872cf17eadf09c1b522869f6758c4
espradli <espradli@trinity.edu>
2021-04-27 11:45:29 -0500

    Made some buttons

--- client/src/main/scala/playscala/Counter.scala
+++ client/src/main/scala/playscala/Counter.scala
@@ -1,21 +1,23 @@
-package edu.trinity.videoquizreact
+package playscala
 
-import shared.SharedMessages
-import org.scalajs.dom
+// import shared.SharedMessages
+// import org.scalajs.dom
 
-import slinky.core._
-import slinky.web.ReactDOM
-import slinky.web.html._
+// import slinky.core._
+// import slinky.web.ReactDOM
+// import slinky.web.html._
+// import scalajs.js
 
-object ScalaJSExample {
-
+object Counter {
   def main(args: Array[String]): Unit = {
+    for(i <- 1 to 10) println(i)
+    
+
     // dom.document.getElementById("scalajsShoutOut").textContent = SharedMessages.itWorks
     // println("Call the react stuff.")
     // ReactDOM.render(
     //   h1("Hello, world!"),
     //   dom.document.getElementById("root")
     // )
-
   }
 }
